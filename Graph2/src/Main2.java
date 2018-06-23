e63dc5b4c08969fb81de4681a47ff4251c9181b3
Pouria Nikvand <pori.niki@gmail.com>
2018-06-23 19:54:45 +0430

    first try
    first try

--- Graph2/src/Main2.java
+++ Graph2/src/Main2.java
@@ -6,41 +6,164 @@ public class Main2 {
 	/**
 	 * @param args
 	 */
+	static int soldierCounts;
+	static int sumA;
+	static int sumB;
+	static int sumC;
 
 	public static void main(String[] args) {
 
 		ArrayList<Soldier> mainArray = new ArrayList<Soldier>();
-		fillArray(mainArray);
-		groupMarking(mainArray);
+		ArrayList<Soldier> gAArray = new ArrayList<Soldier>();
+		ArrayList<Soldier> gBArray = new ArrayList<Soldier>();
+		ArrayList<Soldier> gCArray = new ArrayList<Soldier>();
+
+		fillArray(mainArray, gAArray, gBArray, gCArray);
+		groupMarking(mainArray, gAArray, gBArray, gCArray);
+		System.out.println("A : " + sumA);
+		System.out.println("B : " + sumB);
+		System.out.println("C : " + sumC);
+
 	}
 
-	private static void fillArray(ArrayList<Soldier> mainArray) {
+	private static void fillArray(ArrayList<Soldier> mainArray, ArrayList<Soldier> gAArray, ArrayList<Soldier> gBArray,
+			ArrayList<Soldier> gCArray) {
 
 		ArrayList<Integer> testArray = new ArrayList<Integer>();
 
 		int soldierID = 1;
-		int maxIndex = 0;
-
+		int maxIndex = -1;
+		int maxVal = -1;
+		int maxGroupVal = -1;
+		int maxGroupIndex = -1;
 		try (Scanner scanner = new Scanner(System.in)) {
-			int soldierCounts = scanner.nextInt();
-			int soldierScores = soldierCounts + 3;
+			soldierCounts = scanner.nextInt();
 
 			for (int i = 0; i < soldierCounts; i++) {
-				for (int j = 0; j < soldierScores; j++) {
-					testArray.add(j, scanner.nextInt());
-					if (testArray.get(j)>maxIndex)
-						maxIndex = testArray.get(j);
+				for (int j = 0; j < soldierCounts; j++) {
+					int swap = scanner.nextInt();
+					testArray.add(swap);
+					if (swap > maxVal) {
+						maxVal = swap;
+						maxIndex = j;
+					}
 				}
-				Soldier soldier = new Soldier(testArray, soldierID, maxIndex);
-				mainArray.add(soldierID, soldier);
+				for (int k = 0; k < 3; k++) {
+					int swap = scanner.nextInt();
+					testArray.add(swap);
+					if (swap > maxGroupVal) {
+						maxGroupVal = swap;
+						maxGroupIndex = soldierCounts + k;
+					}
+				}
+				Soldier soldier = new Soldier(testArray, soldierID, maxIndex, maxGroupIndex);
+				// if (maxGroupIndex == 601) {
+				// gAArray.add(soldier);
+				// } else if (maxGroupIndex == 602) {
+				// gBArray.add(soldier);
+				// } else {
+				// gCArray.add(soldier);
+				// }
+				mainArray.add(soldier);
 				soldierID++;
 			}
 		}
 	}
 
-	private static void groupMarking(ArrayList<Soldier> mainArray) {
+	private static void groupMarking(ArrayList<Soldier> mainArray, ArrayList<Soldier> gAArray,
+			ArrayList<Soldier> gBArray, ArrayList<Soldier> gCArray) {
+		int sumtesti = 0;
+		int swapA = 0;
+		int swapB = 0;
+		int swapC = 0;
+		ArrayList<Soldier> noGroup = new ArrayList<Soldier>();
+
 		for (Soldier test : mainArray) {
-			
+			if (test.getGroupNum() != 0)
+				continue;
+			Soldier test2 = mainArray.get(test.getBestIndex() + 1);
+			if (test2.getGroupNum() != 0 && test2.getBestGroupIndex() == test.getBestGroupIndex()) {
+				if (test2.getBestGroupIndex() == 601) {
+					gAArray.add(test);
+					gAArray.add(test2);
+
+				} else if (test2.getBestGroupIndex() == 602) {
+					gBArray.add(test);
+					gBArray.add(test2);
+				} else {
+					gCArray.add(test);
+					gCArray.add(test2);
+				}
+
+				test.setGroupNum(test2.getBestGroupIndex());
+				test2.setGroupNum(test2.getBestGroupIndex());
+
+			} else
+				noGroup.add(test);
 		}
+		for (Soldier aA : gAArray) {
+			for (Soldier aTest : gAArray) {
+				sumA = sumA + aA.getScores().get(aTest.getSoldierID() - 1) * aA.getScores().get(aA.getGroupNum());
+			}
+		}
+		for (Soldier aB : gBArray) {
+			for (Soldier aTest : gBArray) {
+				sumB = sumB + aB.getScores().get(aTest.getSoldierID() - 1) * aB.getScores().get(aB.getGroupNum());
+			}
+		}
+		for (Soldier aC : gCArray) {
+			for (Soldier aTest : gCArray) {
+				sumC = sumC + aC.getScores().get(aTest.getSoldierID() - 1) * aC.getScores().get(aC.getGroupNum());
+			}
+		}
+
+		for (Soldier test : noGroup) {
+			for (Soldier testA : gAArray) {
+				swapA = swapA
+						+ test.getScores().get(testA.getSoldierID() - 1) * test.getScores().get(testA.getGroupNum());
+			}
+			for (Soldier testA : gAArray) {
+				swapB = swapB
+						+ test.getScores().get(testA.getSoldierID() - 1) * test.getScores().get(testA.getGroupNum());
+			}
+			for (Soldier testA : gAArray) {
+				swapC = swapC
+						+ test.getScores().get(testA.getSoldierID() - 1) * test.getScores().get(testA.getGroupNum());
+			}
+			if (swapA > swapB && swapA > swapC) {
+				test.setGroupNum(test.getScores().size() - 2);
+				sumA = swapA + sumA;
+			}
+			if (swapB > swapA && swapB > swapC) {
+				test.setGroupNum(test.getScores().size() - 1);
+				sumB = swapB + sumB;
+			}
+			if (swapC > swapA && swapC > swapB) {
+				test.setGroupNum(test.getScores().size());
+				sumC = swapC + sumC;
+			}
+		}
+
 	}
+	// private static void newDA(ArrayList<Soldier> mainArray,ArrayList<Soldier>
+	// gAArray,ArrayList<Soldier>gBArray,ArrayList<Soldier>gCArray) {
+	//
+	// ArrayList<Soldier> noGroup = new ArrayList<Soldier>();
+	//
+	// for (Soldier test : mainArray) {
+	// if (test.getGroupNum() != 0)
+	// continue;
+	// Soldier test2 = mainArray.get(test.getBestIndex() + 1);
+	// if (test2.getGroupNum() != 0 && test2.getBestGroupIndex() ==
+	// test.getBestGroupIndex()) {
+	// test.setGroupNum(test2.getBestGroupIndex());
+	// test2.setGroupNum(test2.getBestGroupIndex());
+	// }
+	// else
+	// noGroup.add(test);
+	// }
+	// for (Soldier test : noGroup) {
+	// if (test.get)
+	// }
+	// }
 }
--- Graph2/src/Soldier.java
+++ Graph2/src/Soldier.java
@@ -3,23 +3,26 @@ import java.util.ArrayList;
 public class Soldier {
 	
 	
-	private int groupNum;
-	private int groupACount;
-	private int groupBCount;
-	private int groupCCount;
-	private int groupARemain;
-	private int groupBRemain;
-	private int groupCRemain;
+	private int groupNum = 0;
+//	private int groupACount;
+//	private int groupBCount;
+//	private int groupCCount;
+//	private int groupARemain;
+//	private int groupBRemain;
+//	private int groupCRemain;
 	private int soldierID;
 	private int bestIndex;
-	
+	private int bestGroupIndex;
+
 	private ArrayList<Integer> scores;
 
-	public Soldier(ArrayList<Integer> scores, int soldierID, int bestIndex) {
+	public Soldier(ArrayList<Integer> scores, int soldierID, int bestIndex, int bestGroupIndex ) {
 		this.setScores(scores);
 		this.setSoldierID(soldierID);
 		this.setBestIndex(bestIndex);
-
+		this.setBestGroupIndex(bestGroupIndex);
+		// this is for newDA
+		this.setGroupNum(bestGroupIndex);
 	}
 
 	public ArrayList<Integer> getScores() {
@@ -37,54 +40,6 @@ public class Soldier {
 	public void setGroupNum(int groupNum) {
 		this.groupNum = groupNum;
 	}
-	
-	public int getGroupACount() {
-		return groupACount;
-	}
-
-	public void setGroupACount(int groupACount) {
-		this.groupACount = groupACount;
-	}
-
-	public int getGroupBCount() {
-		return groupBCount;
-	}
-
-	public void setGroupBCount(int groupBCount) {
-		this.groupBCount = groupBCount;
-	}
-
-	public int getGroupCCount() {
-		return groupCCount;
-	}
-
-	public void setGroupCCount(int groupCCount) {
-		this.groupCCount = groupCCount;
-	}
-
-	public int getGroupARemain() {
-		return groupARemain;
-	}
-
-	public void setGroupARemain(int groupARemain) {
-		this.groupARemain = groupARemain;
-	}
-
-	public int getGroupBRemain() {
-		return groupBRemain;
-	}
-
-	public void setGroupBRemain(int groupBRemain) {
-		this.groupBRemain = groupBRemain;
-	}
-
-	public int getGroupCRemain() {
-		return groupCRemain;
-	}
-
-	public void setGroupCRemain(int groupCRemain) {
-		this.groupCRemain = groupCRemain;
-	}
 
 	public int getSoldierID() {
 		return soldierID;
@@ -100,6 +55,14 @@ public class Soldier {
 
 	public void setBestIndex(int bestScore) {
 		this.bestIndex = bestScore;
+	}
+
+	public int getBestGroupIndex() {
+		return bestGroupIndex;
+	}
+
+	public void setBestGroupIndex(int bestGroupIndex) {
+		this.bestGroupIndex = bestGroupIndex;
 	}
 
 
